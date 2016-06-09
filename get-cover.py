b3f06a04452afc820c8c5ee5de04977a72082b01
Rafael Sousa <rafael@Mac-Rafmsou.local>
2016-06-09 00:25:42 -0300

    Saving imagem to album folder.

--- get-cover.py
+++ get-cover.py
@@ -1,23 +1,24 @@
 import argparse
 from mutagen import mp4
 
+def save_artwork(image, path):
+  with open(path, 'wb') as img:
+   img.write(artwork)
+
+def get_artwork(filename)
+  try:
+    file = mp4.MP4(filename)
+    albumtitle = file.tags['\xa9alb'][0] if len(file.tags['\xa9alb']) > 0 else 'album'
+    imagepath = '{}/{}.png'.format(filename[0:filename.rfind('/')], albumtitle)
+    
+    if len(file.tags['covr']) > 0:
+      artwork = file.tags['covr'][0]
+      save_artwork(artwork, imagepath)
+  except FileNotFoundError:
+    print('The specified file was not found.')
+
 
 parser = argparse.ArgumentParser(description='Saves embedded artwork (ID3Tag) from a music file.')
 parser.add_argument('filename', metavar='filename', help='The song\'s filename.')
-
 args = parser.parse_args()
-
-try:
-  file = mp4.MP4(args.filename)
-  album_title = file.tags['\xa9alb'][0] if len(file.tags['\xa9alb']) > 0 else 'album'
-  
-  if len(file.tags['covr']) > 0:
-    artwork = file.tags['covr'][0]
-    save_artwork(artwork, album_title)
-except FileNotFoundError:
-  print('The specified file was not found.')
-   
-   
-def save_artwork(image, name):
-  with open(name +'.jpg', 'wb') as img:
-   img.write(artwork)
+get_artwork(args.filename)

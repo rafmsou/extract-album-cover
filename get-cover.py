fc594289cb9094301e84bdc32de3db8dd4ede63d
Rafael Sousa <rafmsou@gmail.com>
2016-06-15 22:50:01 -0300

    Fixing errors.

--- get-cover.py
+++ get-cover.py
@@ -3,14 +3,14 @@ from mutagen import mp4
 
 def save_artwork(image, path):
   with open(path, 'wb') as img:
-   img.write(artwork)
+   img.write(image)
 
-def get_artwork(filename)
+def get_artwork(filename):
   try:
     file = mp4.MP4(filename)
     albumtitle = file.tags['\xa9alb'][0] if len(file.tags['\xa9alb']) > 0 else 'album'
     imagepath = '{}/{}.png'.format(filename[0:filename.rfind('/')], albumtitle)
-    
+
     if len(file.tags['covr']) > 0:
       artwork = file.tags['covr'][0]
       save_artwork(artwork, imagepath)
